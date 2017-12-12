4284d1f7532029d0af056b0799d0e61fc36f7966
LegendCode22 <phoenix22@tutanota.com>
2017-12-12 07:31:19 +0200

    Namespace and 404 Shortcut

--- music/views.py
+++ music/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Album
 # Create your views here.
@@ -12,8 +12,11 @@ def index(request):
     return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
+    """
     try:
         album = Album.objects.get(id=album_id)
     except Album.DoesNotExist:
         raise Http404("Can't find this album!")
+    """
+    album  = get_object_or_404(Album,  id=album_id)
     return render(request, 'music/detail.html', {'album': album})
